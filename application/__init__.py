a2e26f08f62cb92f50568bdd304d76d13a8ae906
Dmitry <budilovdmi@gmail.com>
2023-10-23 08:10:27 +1200

    add some code

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,26 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import json
 
 app = Flask(__name__)
+client = app.test_client()
 
-from . import routes
+tutorials = [
+    {
+        "title": "Video #1. Intro",
+        "description": "GET, POST routes"
+    },
+    {
+        "title": "Video #2. More features",
+        "description": "GET, POST routes"
+    }
+]
+
+@app.get('/tutorials')
+def get_list():
+    return jsonify(tutorials)
+
+@app.post('/tutorials')
+def update_list():
+    new_one = request.json
+    tutorials.append(new_one)
+    return jsonify(tutorials)
